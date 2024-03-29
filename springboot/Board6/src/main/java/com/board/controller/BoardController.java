e83a402096d204c8ae049dbf77d5f36fe2c38ee4
TeckHyeon <rims21@naver.com>
2024-03-29 11:44:08 +0900

    Board6 - 세션, 쿠키

---
+++ springboot/Board6/src/main/java/com/board/controller/BoardController.java
@@ -0,0 +1,122 @@
+package com.board.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.board.domain.BoardVo;
+import com.board.mapper.BoardMapper;
+import com.board.menus.domain.MenuVo;
+import com.board.menus.mapper.MenuMapper;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Controller
+@RequestMapping("/board")
+public class BoardController {
+
+	@Autowired
+	private BoardMapper boardMapper;
+	@Autowired
+	private MenuMapper menuMapper;
+
+	// /Users/List
+	@RequestMapping("/list")
+	public ModelAndView list(MenuVo menuVo) {
+		// 목록 조회
+		List<MenuVo> menuList = menuMapper.getMenuList();
+		List<BoardVo> boardList = boardMapper.getBoardList(menuVo);
+		String menu_id = menuVo.getMenu_id();
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("board/list");
+		mv.addObject("menu_id", menu_id);
+		mv.addObject("boardList", boardList);
+		mv.addObject("menuList", menuList);
+
+		return mv;
+	}
+
+	@RequestMapping("/writeForm")
+	public ModelAndView writeForm(MenuVo menuVo) {
+		ModelAndView mv = new ModelAndView();
+		String menu_id = menuVo.getMenu_id();
+		mv.addObject("menu_id", menu_id);
+		log.info(menu_id);
+		mv.setViewName("board/write");
+		return mv;
+
+	}
+
+	// menu_id = MENU01, title, writer, content
+	@RequestMapping("/write")
+	public ModelAndView write(BoardVo boardVo) {
+		boardMapper.insertBoard(boardVo);
+		String menu_id = boardVo.getMenu_id();
+		ModelAndView mv = new ModelAndView();
+		log.info("boardVo = {}", boardVo);
+		mv.setViewName("redirect:/board/list?menu_id=" + menu_id);
+		return mv;
+	}
+
+	@RequestMapping("/view")
+	public ModelAndView view(BoardVo boardVo) {
+		// 메뉴목록 조회
+				List<MenuVo>  menuList =  menuMapper.getMenuList(); 
+				
+				// 조회수 증가( 현재 bno 의 HIT = HIT + 1 )
+				boardMapper.incHit( boardVo );
+				
+				//  bno 로 조회한 게시글 정보
+				BoardVo       vo       =  boardMapper.viewBoard( boardVo  );   
+				
+				// vo.content 안의 \n 을 '<br>' 로 변경한다
+				String   content  =  vo.getContent();  
+				if(content != null) {
+					content           =  content.replace("\n", "<br>");		
+					vo.setContent(  content  );
+				}
+						
+				ModelAndView  mv  =  new  ModelAndView();
+				mv.addObject("menuList",  menuList );
+				mv.addObject("vo", vo);
+				mv.setViewName("board/view");
+				return  mv;
+	}
+
+	@RequestMapping("/updateform")
+	public ModelAndView updateform(BoardVo boardVo) {
+		ModelAndView mv = new ModelAndView();
+		boardVo = boardMapper.viewBoard(boardVo);
+		String menu_id = boardVo.getMenu_id();
+		mv.addObject("menu_id", menu_id);
+		mv.addObject("board", boardVo);
+		mv.setViewName("board/update");
+		return mv;
+
+	}
+
+	@RequestMapping("/update")
+	public ModelAndView update(BoardVo boardVo) {
+		ModelAndView mv = new ModelAndView();
+		boardMapper.updateboard(boardVo);
+		String menu_id = boardVo.getMenu_id();
+		mv.setViewName("redirect:/board/list?menu_id=" + menu_id);
+		return mv;
+
+	}
+	@RequestMapping("/delete")
+	public ModelAndView delete(BoardVo boardVo) {
+		boardMapper.deleteboard(boardVo);
+		String menu_id = boardVo.getMenu_id();
+		ModelAndView mv = new ModelAndView();
+		log.info("boardVo = {}", boardVo);
+		mv.setViewName("redirect:/board/list?menu_id=" + menu_id);
+		return mv;
+		
+	}
+	
+}
---
+++ springboot/Board6/src/main/webapp/WEB-INF/views/board/list.jsp
@@ -0,0 +1,112 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+<link rel="icon" type="image/png" href="/img/favicon.png" />
+<link rel="stylesheet" href="/css/common.css" />
+<script
+	src="https://cdn.jsdelivr.net/npm/browser-scss@1.0.3/dist/browser-scss.min.js"></script>
+<style>
+table {
+width: 1000px;}
+#board {
+
+td {
+	padding: 10px;
+	width: 150px;
+	text-align: center;
+}
+
+tr:first-child {
+	background-color: black;
+	color: white;
+	font-weight: bold;
+	/* SCSS 문법에 적용 
+     https://www.jsdelivr.com/package/npm/browser-scss
+      */
+	td
+	{
+	border-color
+	:
+	white;
+}
+
+}
+tr:nth-child(2)>td {
+	text-align: right;
+}
+
+td:nth-of-type(1) {
+	width: 80px;
+}
+
+td:nth-of-type(2) {
+	width: 400px;
+}
+
+td:nth-of-type(3) {
+	width: 120px;
+}
+
+td:nth-of-type(4) {
+	width: 180px;
+}
+
+td:nth-of-type(5) {
+	width: 80px;
+}
+
+td:first-child>td {
+	text-align: center;
+}
+}
+
+</style>
+
+</head>
+<body>
+	<main>
+		<%@include file="/WEB-INF/include/menus.jsp" %>
+		<%-- <c:import url="/WEB-INF/include/menus.jsp" ></c:import> --%>
+		<h2>글목록</h2>
+		<table id="board">
+			<tr>
+				<td>번호</td>
+				<td>글제목</td>
+				<td>작성자</td>
+				<td>작성일</td>
+				<td>조회수</td>
+			</tr>
+			<tr>
+				<td colspan="7">[<a href="/board/writeForm?menu_id=${ menu_id }"">글쓰기</a>]
+				</td>
+			</tr>
+
+			<c:forEach var="boardVo" items="${ boardList }">
+				<tr>
+					<td>${ boardVo.bno    }</td>
+					<td><a href="/board/view?bno=${ boardVo.bno }">${ boardVo.title    }</td>
+					<td>${ boardVo.writer    }</td>
+					<td>${ boardVo.regdate    }</td>
+					<td>${ boardVo.hit    }</td>
+				</tr>
+			</c:forEach>
+
+		</table>
+
+	</main>
+</body>
+</html>
+
+
+
+
+
+
+
+
